7866e0da098f0523f25094601316195eb2299bbf
lakivicsa <vicsalaki@gmail.com>
2026-09-14 08:48:47 +0200

    Semicolon delete NekretnineController

--- app/Http/Controllers/NekretninaController.php
+++ app/Http/Controllers/NekretninaController.php
@@ -22,7 +22,7 @@ class NekretninaController extends Controller
 
         return response()->json([
             'status' => true,
-            'podaci' => NekretninaResource::collection(Nekretnina::paginate(10));,
+            'podaci' => NekretninaResource::collection(Nekretnina::paginate(10)),
             'meta' => [
                 'ukupno_stavki' => $nekretnine->count(),
             ]
